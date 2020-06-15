70d6b1b788080d88b747108bf49c447dcfdda94b
Jacob Tyrrell <jacobtyrrell@icloud.com>
2020-06-15 08:22:56 +0100

    try this

--- Sources/BoatsSwift/bot/postBotStats.swift
+++ Sources/BoatsSwift/bot/postBotStats.swift
@@ -1,11 +1,11 @@
 import Just
 extension BoatsSwift {
-	let myJustDefaults = JustSessionDefaults(
-		JSONReadingOptions: .mutableContainers,
-		JSONWritingOptions: .prettyPrinted,
-		headers:  ["Authorization": self.apiKey]
-	)
 	public func postStats(id: String, serverCount: Int) {
+		let myJustDefaults = JustSessionDefaults(
+			JSONReadingOptions: .mutableContainers,
+			JSONWritingOptions: .prettyPrinted,
+			headers:  ["Authorization": self.apiKey]
+		)
 		let jhttp = JustOf<HTTP>(defaults: myJustDefaults)
 		jhttp.post("https://discord.boats/api/bot/\(id)", json:["server_count": serverCount])
 		return
